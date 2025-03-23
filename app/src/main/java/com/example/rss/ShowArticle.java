af708cf165d79d159aa063e71376bb66a78bc561
nezuni1812 <132083873+nezuni1812@users.noreply.github.com>
2025-03-23 13:03:31 +0700

    add icon for each papernews

--- app/src/main/java/com/example/rss/ShowArticle.java
+++ app/src/main/java/com/example/rss/ShowArticle.java
@@ -2,13 +2,17 @@ package com.example.rss;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +26,7 @@ import java.util.ArrayList;
 
 public class ShowArticle extends AppCompatActivity {
     ListView newsList;
+    ImageView logoBao;
     TextView head;
 
     ArrayList<String> headlines;
@@ -34,8 +39,6 @@ public class ShowArticle extends AppCompatActivity {
         Intent callingIntent = getIntent();
         Bundle bun = callingIntent.getExtras();
 
-
-
         head = findViewById(R.id.head);
         newsList = findViewById(R.id.newsList);
 
@@ -46,32 +49,75 @@ public class ShowArticle extends AppCompatActivity {
             newsList.setAdapter(adapter);
         }
 
+        logoBao = findViewById(R.id.logo_bao);
+        String newsName = bun.getString("newsName");
+
+        logoBao.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        if (newsName.equals("Thanh Niên"))
+            logoBao.setImageResource(R.drawable.thanhnien_logo);
+        else if (newsName.equals("Tiền Phong"))
+            logoBao.setImageResource(R.drawable.tienphong_logo);
+        else if (newsName.equals("Vietnamnet"))
+            logoBao.setImageResource(R.drawable.vietnamnet);
+
         newsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(ShowArticle.this);
+
+                LinearLayout titleLayout = new LinearLayout(ShowArticle.this);
+                titleLayout.setOrientation(LinearLayout.HORIZONTAL);
+                titleLayout.setPadding(50, 20, 20, 0);
+                titleLayout.setGravity(Gravity.CENTER_VERTICAL);
+
+                String NewsName = bun.getString("newsName");
+                String Channel = bun.getString("channel");
+
+                ImageView icon = new ImageView(ShowArticle.this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(130, 130); // Width x Height (px)
+                icon.setLayoutParams(layoutParams);
+                icon.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                if (NewsName.equals("Thanh Niên"))
+                    icon.setImageResource(R.drawable.thanhnien_logo);
+                else if (NewsName.equals("Tiền Phong"))
+                    icon.setImageResource(R.drawable.tienphong_logo);
+                else if (NewsName.equals("Vietnamnet"))
+                    icon.setImageResource(R.drawable.vietnamnet);
+
+                icon.setPadding(20, 0, 20, 0);
+
+                TextView title = new TextView(ShowArticle.this);
+                String text = "Items in channel " + Channel + " - " + NewsName;
+                title.setText(text);
+                title.setTextSize(18);
+                title.setTypeface(null, Typeface.BOLD);
+
+                titleLayout.addView(icon);
+                titleLayout.addView(title);
+
                 String desc = allNews.get(i).getDescription().replaceAll("￼", "");
                 String formattedMessage = "<b>" + allNews.get(i).getTitle() + "</b><br><br>" + desc;
-                builder.setTitle(bun.getString("caption")).setMessage(Html.fromHtml(formattedMessage, Html.FROM_HTML_MODE_LEGACY));
+
+                builder.setCustomTitle(titleLayout);
+                builder.setMessage(Html.fromHtml(formattedMessage, Html.FROM_HTML_MODE_LEGACY));
+
                 builder.setPositiveButton("More", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // Toast.makeText(ShowArticle.this, "More", Toast.LENGTH_SHORT).show();
-                        adapterView.getItemIdAtPosition(i);
                         Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse(allNews.get(i).getLink()));
                         startActivity(browser);
                     }
                 });
+
                 builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // Toast.makeText(ShowArticle.this, "Close", Toast.LENGTH_SHORT).show();
                         dialog.dismiss();
                     }
                 });
+
                 builder.create();
                 builder.show();
-
             }
         });
 
--- app/src/main/java/com/example/rss/ShowNews.java
+++ app/src/main/java/com/example/rss/ShowNews.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 
 public class ShowNews extends AppCompatActivity {
     ListView channelList;
+    ImageView logoBao;
     TextView tieu;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -24,9 +27,19 @@ public class ShowNews extends AppCompatActivity {
         Intent callingIntent = getIntent();
         Bundle bun = callingIntent.getExtras();
 //        urlAddress = myBundle.getString("urlAddress"); urlCaption = myBundle.getString(“urlCaption”);
-
+        logoBao = findViewById(R.id.logo_bao);
         tieu = findViewById(R.id.tieu);
-        tieu.setText("Channels in " + bun.getString("newsName"));
+        String newsName = bun.getString("newsName");
+        String title = "Channels in " + newsName;
+        tieu.setText(title);
+
+        logoBao.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        if (newsName.equals("Thanh Niên"))
+            logoBao.setImageResource(R.drawable.thanhnien_logo);
+        else if (newsName.equals("Tiền Phong"))
+            logoBao.setImageResource(R.drawable.tienphong_logo);
+        else if (newsName.equals("Vietnamnet"))
+            logoBao.setImageResource(R.drawable.vietnamnet);
 
         ArrayList<String> channels = bun.getStringArrayList("channelNames");
         ArrayList<String> urls = bun.getStringArrayList("channelUrl");
@@ -42,7 +55,9 @@ public class ShowNews extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent callShowIndividualNews = new Intent(ShowNews.this, ShowArticle.class);
                 Bundle myData = new Bundle();
-                myData.putString("caption", "Items in Channel " + channels.get(i) + " - "  +  bun.getString("newsName"));
+                myData.putString("caption", "Items in channel " + channels.get(i) + " - "  +  bun.getString("newsName"));
+                myData.putString("channel", channels.get(i));
+                myData.putString("newsName", bun.getString("newsName"));
                 System.out.println(urls.get(i));
                 myData.putString("url", urls.get(i));
                 callShowIndividualNews.putExtras(myData);
